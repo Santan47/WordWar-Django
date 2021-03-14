c18eccd24d394ee42beebc4f179cbdcecdf20316
santan47 <santankumarsharma47@gmail.com>
2021-03-14 23:48:42 +0530

    model updated

--- testapp/models.py
+++ testapp/models.py
@@ -3,6 +3,13 @@ from django.db import models
 class tb_content(models.Model):
     s_no = models.AutoField(primary_key=True)
     userid = models.IntegerField()
+    Title= models.CharField(max_length=1000)
+    content= models.TextField()
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+
+class tb_content(models.Model):
+    s_no = models.AutoField(primary_key=True)
+    userid = models.IntegerField()
     user_name = models.CharField(max_length=1000)
     profile_img = models.ImageField(upload_to="pics")
     Title= models.CharField(max_length=1000)
