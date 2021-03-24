613395fb154749528a9bb6ef0b55bd9033a3d58f
Santan kr Sharma <santankumarsharma47@gmail.com>
2021-03-24 11:10:55 +0530

    Change

--- testapp/models.py
+++ testapp/models.py
@@ -6,7 +6,7 @@ class tb_content(models.Model):
     Title= models.CharField(max_length=1000)
     content= models.TextField()
     created = models.DateTimeField(auto_now_add=True, blank=True)
-#update goes here 3
+#update goes here 4
 # class tb_content(models.Model):
 #     s_no = models.AutoField(primary_key=True)
 #     userid = models.IntegerField()
